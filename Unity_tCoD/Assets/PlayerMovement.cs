acb3b2f0dc8f8239fabaa3532992214702fbd259
AnnLoseva <95305121+AnnLoseva@users.noreply.github.com>
2024-04-16 09:31:55 +0300

    WallSlide

--- Unity_tCoD/Assets/PlayerMovement.cs
+++ Unity_tCoD/Assets/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,23 +9,33 @@ public class PlayerMovement : MonoBehaviour
     private BoxCollider2D coll; // Collistion of Player
     private Animator anim; //Animations
     private float dirX = 0f;  // Movement input
-    private float dirY = 0f;  // Movement input
+    //private float dirY = 0f;  // Movement input
     private SpriteRenderer sprite; // 
     private int jumpCount = 0; // Count of jumps in air
+    private bool isWallSliding = false; // Check of WallSliding
+
 
 
     [SerializeField] private AudioSource jumpSoundEffect;
     [SerializeField] private LayerMask jumpableGround; // Ground Collision
     [SerializeField] private LayerMask climbableWall; // Ground Collision
+    [SerializeField] private Transform wallCheck; // Point of wallCheck
 
     [SerializeField] private float moveSpeed = 7f; // Movement Speed
     [SerializeField] private float jumpForce = 20f; // Jump Force
-    [SerializeField] private int airJumpMaxNum = 1; // Max number of air jumps
+
     [SerializeField] private float airJumpForce = 15f; // Force of jumps in air
+    [SerializeField] private int airJumpMaxNum = 1; // Max number of air jumps
+
     [SerializeField] private float floatGravity = 2f; //Gravity while floating
     [SerializeField] private float regularGravity = 4f; // Regular gravity
-    [SerializeField] private float wallJumpHeight = 10f;
-    [SerializeField] private float wallJumpDirection = 10f;
+
+    [SerializeField] private float wallSlidingSpeed = 2f; // Speed of Wall Slide
+
+    
+
+
+    
     private enum MovementState { idle, running, jumping, falling } // Animation States
 
 
@@ -47,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
 
         UpdateAnimationState();// Calling UpdateAnimationState
         JumpsAndFloating();
-        WallClimb();
+        WallSlide();
     }
 
     // Animation Run/Idle
@@ -128,46 +139,42 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void WallClimb()
+    private void WallSlide()
     {
-        if( Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.left, 0.3f, climbableWall) || Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, 0.3f, jumpableGround))
+        if(!IsGrounded() && IsWalled())
         {
-            rb.gravityScale = floatGravity;
-            dirY = Input.GetAxisRaw("Vertical");
-
-            //Moving Horizontaly
-            if (dirY > 0.1f|| dirY < -0.1f)
-            {
-                print("Moving");
-                rb.velocity = new Vector2(rb.velocity.x, dirY * moveSpeed);
-            } else { rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y); }
-
-            if(Input.GetButtonDown("Jump"))
-            {
-                if (Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.left, 0.3f, climbableWall))
-                    { 
-                    
-                    rb.velocity = new Vector2(wallJumpDirection, wallJumpHeight);
-                    print("left: " + rb.velocity);
-                }else if (Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, 0.3f, climbableWall))
-                {
-                   
-                    rb.velocity = new Vector2(-wallJumpDirection, wallJumpHeight); 
-                    print("right" + rb.velocity);
-                }
-
-            }
-
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            //rb.gravityScale = 0;
         }
         else
         {
-            rb.gravityScale = regularGravity;
+            isWallSliding = false;
+            //rb.gravityScale = regularGravity;
         }
     }
+
 
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.3f, jumpableGround);
+    }
+
+    private bool IsWalled()
+    {
+        bool isWalled = false;
+
+        if( Physics2D.OverlapCircle(wallCheck.position, 0.2f, jumpableGround))
+        {
+            isWalled = true;
+        };
+
+        if(Physics2D.OverlapCircle(wallCheck.position, 0.2f, jumpableGround))
+        {
+            isWalled = true;
+        };
+
+        return isWalled;
     }
 
 }
